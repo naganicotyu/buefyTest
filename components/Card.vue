6aeab9f691a81a605b8b3b13f6a8fd1e79b2d123
naganicotyu <niconico.guruhirorin.324@gmail.com>
2023-12-28 13:18:33 +0900

    modified:   components/Card.vue

--- components/Card.vue
+++ components/Card.vue
@@ -10,11 +10,11 @@
         <b-field label="下記のフォームに入力をしてください。">
             <b-input v-model="name"></b-input>
         </b-field>
-        <template>
+
     <section>
-        <b-button @click="clickMe" class="content has-text-right">Click Me</b-button>
+        <b-button  @click="clickMe" >Click Me</b-button>
     </section>
-</template>
+
         <div class="content has-text-centered">
           <b-icon :icon="icon" size="is-large" type="is-primary" />
         </div>
